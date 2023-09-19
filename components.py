1a0e54f84c48a8e194c157dbd14f9d3a37bc4c6a
Henry Glenn <hgglenn518@gmail.com>
2023-09-19 17:54:29 -0500

    Added a way for the Scene class to access different scenes and set the current scene of a Window class

--- components.py
+++ components.py
@@ -1,3 +1,5 @@
+from typing import Type
+
 import OpenGL.GL as opengl
 import OpenGL.GLU as openglu
 import glfw as openglfw
@@ -6,16 +8,26 @@ import json
 import os
 
 
+# NOTE: Scenes should always be self-contained with
 class Scene:
-    def __init__(self, window) -> None:
+    def __init__(self, window: "Window") -> None:
         self.window = window
+        self.is_setup = False
+
+    def get_window(self):
+        return self.window
 
     def setup(self) -> None:
         pass
 
     def call(self) -> None:
-        self.update()
-        self.draw()
+        if self.is_setup:
+            self.update()
+            self.draw()
+        else:
+            self.setup()
+            self.is_setup = True
+            self.call()
 
     def update(self) -> None:
         pass
@@ -25,42 +37,66 @@ class Scene:
 
 
 class Window:
-    def __init__(self, size: tuple = (1000, 800), title: str = "New Window", fps: int = 60) -> None:
+    def __init__(self, *args, **kwargs) -> None:
+        self.fps = 60
+        self.scenes = {}
+        self.current_scene = None
+
         # Make sure OpenGL framework can initialize
         if not openglfw.init():
             raise ImportError("Graphics Library Framework could not initialize")
 
         # Create graphics window
-        self.graphics_window = openglfw.create_window(*size, title, None, None)
-        if not self.graphics_window:
+        self.OPENGL_WINDOW = openglfw.create_window(500, 500, "OpenGL Window", None, None)
+        if not self.OPENGL_WINDOW:
             openglfw.terminate()
             raise Exception("Graphics window creation failed.")
 
-        openglfw.make_context_current(self.graphics_window)
-        openglfw.set_window_size_callback(self.graphics_window, self.resize_event)
-        openglfw.set_window_close_callback(self.graphics_window, self.close_event)
+        openglfw.make_context_current(self.OPENGL_WINDOW)
+        openglfw.set_window_size_callback(self.OPENGL_WINDOW, self.resize_event)
+        openglfw.set_window_close_callback(self.OPENGL_WINDOW, self.close_event)
 
-        self.fps = fps
-        self.current_scene = None
+    def set_title(self, title: str) -> None:
+        openglfw.set_window_title(self.OPENGL_WINDOW, title)
 
-    def get_graphics_window(self):
-        return self.graphics_window
-
-    def set_frames_per_second(self, fps: int) -> None:
+    def set_fps(self, fps: int) -> None:
         self.fps = fps
 
     def get_frame_interval(self) -> float:
         return 1 / self.fps
 
-    def set_current_scene(self, scene: Scene) -> None:
-        self.current_scene = scene
-        self.get_current_scene().setup()
+    def set_input_mode(self, input_type: int, option: int):
+        openglfw.set_input_mode(self.OPENGL_WINDOW, input_type, option)
+
+    def get_size(self) -> tuple[int, int]:
+        return openglfw.get_framebuffer_size(self.OPENGL_WINDOW)
+
+    def set_size(self, size: tuple[int, int]) -> None:
+        openglfw.set_window_size(self.OPENGL_WINDOW, size[0], size[1])
+
+    def get_key(self, key: int) -> bool:
+        return True if openglfw.get_key(self.OPENGL_WINDOW, key) else False
+
+    def get_cursor_position(self) -> tuple[int, int]:
+        return openglfw.get_cursor_pos(self.OPENGL_WINDOW)
+
+    def set_cursor_position(self, x: int, y: int) -> None:
+        openglfw.set_cursor_pos(self.OPENGL_WINDOW, x, y)
+
+    def add_scene_reference(self, scene_name: str, scene_reference: Type[Scene]) -> None:
+        self.scenes[scene_name] = scene_reference
+
+    def get_scene_reference(self, scene_name: str) -> Type[Scene]:
+        return self.scenes[scene_name]
+
+    def set_current_scene(self, scene_reference: Type[Scene]) -> None:
+        self.current_scene = scene_reference(self)
 
     def get_current_scene(self) -> Scene:
         return self.current_scene
 
     def mainloop(self) -> None:
-        while not openglfw.window_should_close(self.graphics_window):
+        while not openglfw.window_should_close(self.OPENGL_WINDOW):
             # Get the time at the start of the frame
             frame_start_time = time.time()
 
@@ -82,12 +118,6 @@ class Window:
 
     def resize_event(self, window, width: int, height: int) -> None:
         opengl.glViewport(0, 0, width, height)
-        opengl.glMatrixMode(opengl.GL_PROJECTION)
-        opengl.glLoadIdentity()
-        openglu.gluPerspective(45, (width / max(1, height)), 0.1, 50.0)
-        opengl.glTranslatef(0.0, 0.0, -5)
-        opengl.glMatrixMode(opengl.GL_MODELVIEW)
-        opengl.glLoadIdentity()
 
     def close_event(self, window) -> None:
         pass
@@ -97,13 +127,10 @@ class Window:
         opengl.glClear(opengl.GL_COLOR_BUFFER_BIT | opengl.GL_DEPTH_BUFFER_BIT)
 
         # If the current scene is set then call it
-        if self.get_current_scene() is None:
-            raise TypeError("Current scene has not been set")
-        else:
-            self.get_current_scene().call()
+        self.get_current_scene().call()
 
         # Show current frame
-        openglfw.swap_buffers(self.graphics_window)
+        openglfw.swap_buffers(self.OPENGL_WINDOW)
 
 
 class LocalStorage(dict):
--- main.py
+++ main.py
@@ -1,32 +1,28 @@
 import math
-import random
 import settings
-import components
+from components import *
 import OpenGL.GL as opengl
 import OpenGL.GLU as openglu
 import glfw as openglfw
 
 
-class TestScene(components.Scene):
-    def __init__(self, window):
+class RenderTest(Scene):
+    def __init__(self, window: Window):
         super().__init__(window=window)
         self.cube_vertices = ((0, 0, 0), (0, 0, 1), (0, 1, 1), (0, 1, 0),
                               (1, 0, 0), (1, 1, 1), (1, 1, 0), (1, 0, 1))
         self.cube_edges = ((0, 1), (0, 3), (0, 4), (1, 2), (1, 7), (2, 5),
                            (2, 3), (3, 6), (4, 6), (4, 7), (5, 6), (5, 7))
-        #self.cube_faces = ((0, 3, 6, 4), (2, 5, 6, 3), (1, 2, 5, 7),
-        #                   (1, 0, 4, 7), (7, 4, 6, 5), (2, 3, 0, 1))
-
         self.cube_faces = ((0, 3, 6, 4), (2, 5, 6, 3), (7, 5, 2, 1),
                            (1, 0, 4, 7), (7, 4, 6, 5), (2, 3, 0, 1))
         self.position = [0.0, 0.0, 0.0]
         self.angle = [0.0, 0.0, 0.0]
 
     def setup(self) -> None:
-        width, height = openglfw.get_framebuffer_size(self.window)
+        width, height = self.get_window().get_size()
 
-        openglfw.set_input_mode(self.window, openglfw.CURSOR, openglfw.CURSOR_DISABLED)
-        openglfw.set_cursor_pos(self.window, round(width / 2), round(height / 2))
+        self.get_window().set_input_mode(openglfw.CURSOR, openglfw.CURSOR_DISABLED)
+        self.get_window().set_cursor_position(width // 2, height // 2)
 
         # Enable back-face culling
         opengl.glEnable(opengl.GL_CULL_FACE)
@@ -37,29 +33,30 @@ class TestScene(components.Scene):
         opengl.glEnable(opengl.GL_DEPTH_TEST)
 
     def update(self) -> None:
-        width, height = openglfw.get_framebuffer_size(self.window)
+        width, height = self.get_window().get_size()
 
-        cursor_velocity = self.get_cursor_velocity()
+        cursor_position = self.get_window().get_cursor_position()
+        cursor_velocity = (cursor_position[0] - (width // 2), cursor_position[1] - (height // 2))
         self.angle[0] = (self.angle[0] + cursor_velocity[0] / 8) % 360
         self.angle[1] = max(min(90.0, self.angle[1] + cursor_velocity[1] / 8), -90.0)
+        self.get_window().set_cursor_position(width // 2, height // 2)
 
-        self.set_cursor_to_center()
-        if openglfw.get_key(self.window, openglfw.KEY_W):
+        if self.get_window().get_key(openglfw.KEY_W):
             self.position[0] -= math.sin((self.angle[0] / 180) * math.pi) * 0.1
             self.position[2] += math.cos((self.angle[0] / 180) * math.pi) * 0.1
-        if openglfw.get_key(self.window, openglfw.KEY_S):
+        if self.get_window().get_key(openglfw.KEY_S):
             self.position[0] += math.sin((self.angle[0] / 180) * math.pi) * 0.1
             self.position[2] -= math.cos((self.angle[0] / 180) * math.pi) * 0.1
-        if openglfw.get_key(self.window, openglfw.KEY_A):
+        if self.get_window().get_key(openglfw.KEY_A):
             self.position[0] += math.cos((self.angle[0] / 180) * math.pi) * 0.1
             self.position[2] += math.sin((self.angle[0] / 180) * math.pi) * 0.1
-        if openglfw.get_key(self.window, openglfw.KEY_D):
+        if self.get_window().get_key(openglfw.KEY_D):
             self.position[0] -= math.cos((self.angle[0] / 180) * math.pi) * 0.1
             self.position[2] -= math.sin((self.angle[0] / 180) * math.pi) * 0.1
 
-        if openglfw.get_key(self.window, openglfw.KEY_SPACE):
+        if self.get_window().get_key(openglfw.KEY_SPACE):
             self.position[1] -= 0.1
-        if openglfw.get_key(self.window, openglfw.KEY_LEFT_SHIFT):
+        if self.get_window().get_key(openglfw.KEY_LEFT_SHIFT):
             self.position[1] += 0.1
 
         opengl.glLoadIdentity()
@@ -68,15 +65,6 @@ class TestScene(components.Scene):
         opengl.glRotatef(self.angle[1], True, False, False)
         opengl.glRotatef(self.angle[0], False, True, False)
         opengl.glTranslatef(*self.position)
-
-    def get_cursor_velocity(self) -> tuple[int, int]:
-        width, height = openglfw.get_framebuffer_size(self.window)
-        mouse_x, mouse_y = openglfw.get_cursor_pos(self.window)
-        return int(mouse_x - width / 2), int(mouse_y - height / 2)
-
-    def set_cursor_to_center(self) -> None:
-        width, height = openglfw.get_framebuffer_size(self.window)
-        openglfw.set_cursor_pos(self.window, round(width / 2), round(height / 2))
 
     def draw_cube(self, x: float, y: float, z: float) -> None:
         opengl.glBegin(opengl.GL_QUADS)
@@ -100,20 +88,14 @@ class TestScene(components.Scene):
         self.draw_cube(2, 0, 2)
 
 
-
-
-class GameWindow(components.Window):
-    def __init__(self) -> None:
-        super().__init__(size=settings.local_storage["base_window_size"],
-                         title=settings.WINDOW_TITLE,
-                         fps=settings.local_storage["fps"])
-        # Initialize scenes
-        self.scene1 = TestScene(self.get_graphics_window())
-
-        # Set initial scene
-        self.set_current_scene(self.scene1)
-
-
 if __name__ == "__main__":
-    game = GameWindow()
+    game = Window()
+
+    game.set_size(settings.local_storage["base_window_size"])
+    game.set_title(settings.WINDOW_TITLE)
+    game.set_fps(settings.local_storage["fps"])
+
+    game.add_scene_reference("test-0", RenderTest)
+
+    game.set_current_scene(game.get_scene_reference("test-0"))
     game.mainloop()
